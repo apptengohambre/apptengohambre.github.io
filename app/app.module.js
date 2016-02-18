5153d7ac6cf9bac3e3fc46d466f13ff626a04737
Paul Vidal <i@paulvl.me>
2016-02-18 12:53:18 -0500

    arreglo de vistas cacheadas

--- app/app.module.js
+++ app/app.module.js
@@ -1,1 +1,9 @@
 var app = angular.module('tengoHambreApp', ['ui.router', 'oc.lazyLoad']);
+
+app.run(function($rootScope, $templateCache) {
+    $rootScope.$on('$routeChangeStart', function(event, next, current) {
+        if (typeof(current) !== 'undefined'){
+            $templateCache.remove(current.templateUrl);
+        }
+    });
+});
